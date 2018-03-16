a9cf05c5ea6667b94eec373bdda866de46ff4682
Adam Furtenbach <adam.furtenbach@gmail.com>
2018-03-16 12:07:03 +0100

    .

--- ESPlus.IntegrationTests/RepostoryTests.cs
+++ ESPlus.IntegrationTests/RepostoryTests.cs
@@ -9,115 +9,115 @@ using System;
 
 namespace ESPlus.IntegrationTests
 {
-    public class HashEvent
-    {
-        public string Checksum { get; set; }
-    }
+    // public class HashEvent
+    // {
+    //     public string Checksum { get; set; }
+    // }
 
-    public class ChecksumAggregate : ReplayableObject
-    {
-        public string Checksum { get; set; }
+    // public class ChecksumAggregate : ReplayableObject
+    // {
+    //     public string Checksum { get; set; }
 
-        public ChecksumAggregate(string id)
-            : base(id)
-        {
-            Checksum = Hash(id);
-        }
+    //     public ChecksumAggregate(string id)
+    //         : base(id)
+    //     {
+    //         Checksum = Hash(id);
+    //     }
 
-        public void TriggerEvent()
-        {
-            ApplyChange(new HashEvent
-            {
-                Checksum = Hash(Checksum)
-            });
-        }
+    //     public void TriggerEvent()
+    //     {
+    //         ApplyChange(new HashEvent
+    //         {
+    //             Checksum = Hash(Checksum)
+    //         });
+    //     }
 
-        protected void Apply(HashEvent @event)
-        {
-            Checksum = @event.Checksum;
-        }
+    //     protected void Apply(HashEvent @event)
+    //     {
+    //         Checksum = @event.Checksum;
+    //     }
 
-        private static string Hash(string input)
-        {
-            using (var provider = System.Security.Cryptography.SHA1.Create())
-            {
-                var hash = provider.ComputeHash(Encoding.UTF8.GetBytes(input));
-                return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
-            }
-        }
-    }
+    //     private static string Hash(string input)
+    //     {
+    //         using (var provider = System.Security.Cryptography.SHA1.Create())
+    //         {
+    //             var hash = provider.ComputeHash(Encoding.UTF8.GetBytes(input));
+    //             return string.Join("", hash.Select(b => b.ToString("x2")).ToArray());
+    //         }
+    //     }
+    // }
 
-    public class RepostoryTests
-    {
-        private IRepository _repository;
-        private ChecksumAggregate _aggregate;
-        private string _id;
+    // public class RepostoryTests
+    // {
+    //     private IRepository _repository;
+    //     private ChecksumAggregate _aggregate;
+    //     private string _id;
 
-        public RepostoryTests()
-        {
-            var connectionString = "ConnectTo=tcp://admin:changeit@localhost:1113; HeartBeatTimeout=500";
-            var eventStoreConnection = EventStoreConnection.Create(connectionString);
-            eventStoreConnection.ConnectAsync().Wait();
-            _repository = new GetEventStoreRepository(eventStoreConnection, new EventJsonSerializer());
-            _id = Guid.NewGuid().ToString();
-            _aggregate = new ChecksumAggregate(_id);
+    //     public RepostoryTests()
+    //     {
+    //         var connectionString = "ConnectTo=tcp://admin:changeit@localhost:1113; HeartBeatTimeout=500";
+    //         var eventStoreConnection = EventStoreConnection.Create(connectionString);
+    //         eventStoreConnection.ConnectAsync().Wait();
+    //         _repository = new GetEventStoreRepository(eventStoreConnection, new EventJsonSerializer());
+    //         _id = Guid.NewGuid().ToString();
+    //         _aggregate = new ChecksumAggregate(_id);
 
-            GetEventStoreRepository.Register<HashEvent>();
-        }
+    //         GetEventStoreRepository.Register<HashEvent>();
+    //     }
 
-        [Fact]
-        public void Save_NoEvents_Pass()
-        {
-            _repository.SaveAsync(_aggregate);
-        }
+    //     [Fact]
+    //     public void Save_NoEvents_Pass()
+    //     {
+    //         _repository.SaveAsync(_aggregate);
+    //     }
 
-        [Fact]
-        public void Save_OneEvent_Pass()
-        {
-            _aggregate.TriggerEvent();
-            _repository.SaveAsync(_aggregate);
-        }
+    //     [Fact]
+    //     public void Save_OneEvent_Pass()
+    //     {
+    //         _aggregate.TriggerEvent();
+    //         _repository.SaveAsync(_aggregate);
+    //     }
 
-        [Fact(Skip = "Fails for some reason")]
-        public void Save_AppendEvents_Pass()
-        {
-            _aggregate.TriggerEvent();
-            _repository.SaveAsync(_aggregate);
-            _aggregate.TriggerEvent();
-            _repository.SaveAsync(_aggregate);
-        }
+    //     [Fact(Skip = "Fails for some reason")]
+    //     public void Save_AppendEvents_Pass()
+    //     {
+    //         _aggregate.TriggerEvent();
+    //         _repository.SaveAsync(_aggregate);
+    //         _aggregate.TriggerEvent();
+    //         _repository.SaveAsync(_aggregate);
+    //     }
 
-        [Fact]
-        public void GetById_LoadSaveAggregate_ReplayedToPreviousState()
-        {
-            Repeat(() => _aggregate.TriggerEvent(), times: 1024);
-            var checksum = _aggregate.Checksum;
-            var version = _aggregate.Version;
+    //     [Fact]
+    //     public void GetById_LoadSaveAggregate_ReplayedToPreviousState()
+    //     {
+    //         Repeat(() => _aggregate.TriggerEvent(), times: 1024);
+    //         var checksum = _aggregate.Checksum;
+    //         var version = _aggregate.Version;
 
-            _repository.SaveAsync(_aggregate);
-            var result = _repository.GetByIdAsync<ChecksumAggregate>(_id);
+    //         _repository.SaveAsync(_aggregate);
+    //         var result = _repository.GetByIdAsync<ChecksumAggregate>(_id);
 
-            Assert.Equal(checksum, result.Checksum);
-            Assert.Equal(version, result.Version);
-        }
+    //         Assert.Equal(checksum, result.Checksum);
+    //         Assert.Equal(version, result.Version);
+    //     }
 
-        [Fact]
-        public void Save_ReloadAggregateAndResave_Pass()
-        {
-            _aggregate.TriggerEvent();
-            _repository.SaveAsync(_aggregate);
+    //     [Fact]
+    //     public void Save_ReloadAggregateAndResave_Pass()
+    //     {
+    //         _aggregate.TriggerEvent();
+    //         _repository.SaveAsync(_aggregate);
 
-            var aggregate = _repository.GetByIdAsync<ChecksumAggregate>(_id);
-            aggregate.TriggerEvent();
-            _repository.SaveAsync(aggregate);
-        }
+    //         var aggregate = _repository.GetByIdAsync<ChecksumAggregate>(_id);
+    //         aggregate.TriggerEvent();
+    //         _repository.SaveAsync(aggregate);
+    //     }
 
-        private void Repeat(Action action, int times)
-        {
-            for (var i = 0; i < times; ++i)
-            {
-                action();
-            }
-        }
-    }
+    //     private void Repeat(Action action, int times)
+    //     {
+    //         for (var i = 0; i < times; ++i)
+    //         {
+    //             action();
+    //         }
+    //     }
+    // }
 }
--- ESPlus.Tests/SpecificationTests.cs
+++ ESPlus.Tests/SpecificationTests.cs
@@ -4,140 +4,140 @@ using Xunit;
 
 namespace ESPlus.Tests
 {
-    public class DummyAggregate : ReplayableObject
-    {
-        public DummyAggregate(bool emit)
-            : base("N/A")
-        {
-            ApplyChange(new DummyEvent
-            {
-                Explanation = "Created"
-            });
-        }
+    // public class DummyAggregate : ReplayableObject
+    // {
+    //     public DummyAggregate(bool emit)
+    //         : base("N/A")
+    //     {
+    //         ApplyChange(new DummyEvent
+    //         {
+    //             Explanation = "Created"
+    //         });
+    //     }
 
-        public void Emit()
-        {
-            ApplyChange(new DummyEvent
-            {
-                Explanation = "Emit"
-            });
-        }
-    }
+    //     public void Emit()
+    //     {
+    //         ApplyChange(new DummyEvent
+    //         {
+    //             Explanation = "Emit"
+    //         });
+    //     }
+    // }
 
-    public class DummyEvent
-    {
-        public string Explanation { get; set; }
-    }
+    // public class DummyEvent
+    // {
+    //     public string Explanation { get; set; }
+    // }
 
-    public class SpecificationTests : ESPlus.Specification.Specification<DummyAggregate>
-    {
-        private Exception _exception;
+    // public class SpecificationTests : ESPlus.Specification.Specification<DummyAggregate>
+    // {
+    //     private Exception _exception;
 
-        protected override DummyAggregate Create()
-        {
-            if (_exception != null)
-            {
-                throw _exception;
-            }
+    //     protected override DummyAggregate Create()
+    //     {
+    //         if (_exception != null)
+    //         {
+    //             throw _exception;
+    //         }
 
-            return new DummyAggregate(false);
-        }
+    //         return new DummyAggregate(false);
+    //     }
 
-        [Fact]
-        public void Constructor_Create_Emit()
-        {
-            Then(() =>
-            {
-                Is<DummyEvent>(p => p.Explanation == "Created");
-            });
-        }
+    //     [Fact]
+    //     public void Constructor_Create_Emit()
+    //     {
+    //         Then(() =>
+    //         {
+    //             Is<DummyEvent>(p => p.Explanation == "Created");
+    //         });
+    //     }
 
-        [Fact]
-        public void Constructor_CreateBadMatch_Throws()
-        {
-            Assert.Throws<Exception>(() =>
-            {
-                Then(() =>
-                {
-                    Is<DummyEvent>(p => p.Explanation == "NOT Created");
-                });
-            });
-        }
+    //     [Fact]
+    //     public void Constructor_CreateBadMatch_Throws()
+    //     {
+    //         Assert.Throws<Exception>(() =>
+    //         {
+    //             Then(() =>
+    //             {
+    //                 Is<DummyEvent>(p => p.Explanation == "NOT Created");
+    //             });
+    //         });
+    //     }
 
-        [Fact]
-        public void Constructor_EmptyWhen_Nothing()
-        {
-            When(() => { });
+    //     [Fact]
+    //     public void Constructor_EmptyWhen_Nothing()
+    //     {
+    //         When(() => { });
 
-            ThenNothing();
-        }
+    //         ThenNothing();
+    //     }
 
-        [Fact]
-        public void Constructor_Throws_Propagate()
-        {
-            _exception = new Exception();
+    //     [Fact]
+    //     public void Constructor_Throws_Propagate()
+    //     {
+    //         _exception = new Exception();
 
-            Assert.Throws<Exception>(() =>
-            {
-                When(() =>
-                {
+    //         Assert.Throws<Exception>(() =>
+    //         {
+    //             When(() =>
+    //             {
 
-                });
+    //             });
 
-                //ThenNothing();
-            });
-        }
+    //             //ThenNothing();
+    //         });
+    //     }
 
-        [Fact]
-        public void When_Throws_Propagate()
-        {
-            Assert.Throws<Exception>(() =>
-            {
-                When(() =>
-                {
-                    throw new Exception();
-                });
+    //     [Fact]
+    //     public void When_Throws_Propagate()
+    //     {
+    //         Assert.Throws<Exception>(() =>
+    //         {
+    //             When(() =>
+    //             {
+    //                 throw new Exception();
+    //             });
 
-                ThenNothing();
-            });
-        }
+    //             ThenNothing();
+    //         });
+    //     }
 
-        [Fact]
-        public void Constructor_Throws_Capture()
-        {
-            _exception = new Exception();
+    //     [Fact]
+    //     public void Constructor_Throws_Capture()
+    //     {
+    //         _exception = new Exception();
 
-            ThenThrows<Exception>();
-        }
+    //         ThenThrows<Exception>();
+    //     }
 
-        [Fact]
-        public void When_Throws_Capture()
-        {
-            When(() =>
-            {
-                throw new Exception();
-            });
+    //     [Fact]
+    //     public void When_Throws_Capture()
+    //     {
+    //         When(() =>
+    //         {
+    //             throw new Exception();
+    //         });
 
-            ThenThrows<Exception>();
-        }
+    //         ThenThrows<Exception>();
+    //     }
 
-        [Fact]
-        public void Then_MoreEmittedThanCapturedEvents_Throws()
-        {
-            var exception = Assert.Throws<Exception>(() =>
-            {
-                When(() =>
-                {
-                    Aggregate.Emit();
-                });
+    //     [Fact]
+    //     public void Then_MoreEmittedThanCapturedEvents_Throws()
+    //     {
+    //         var exception = Assert.Throws<Exception>(() =>
+    //         {
+    //             When(() =>
+    //             {
+    //                 Aggregate.Emit();
+    //             });
 
-                Then(() =>
-                {
-                    Is<object>();
-                });
-            });
+    //             Then(() =>
+    //             {
+    //                 Is<object>();
+    //             });
+    //         });
 
-            //Assert.Equal("No more events!", exception.Message);
-        }
-    }
+    //         //Assert.Equal("No more events!", exception.Message);
+    //     }
+    // }
 }
