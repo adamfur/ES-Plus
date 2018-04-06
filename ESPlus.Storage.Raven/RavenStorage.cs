9c5d18f4ef9876b1b15cba8c1644688ec709227e
Adam Furtenbach <adam.furtenbach@gmail.com>
2018-04-06 17:28:06 +0200

    .

--- ESPlus.Storage.Raven/RavenStorage.cs
+++ ESPlus.Storage.Raven/RavenStorage.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using ESPlus.Interfaces;
 using Raven.Client.Documents;
 
@@ -8,6 +9,7 @@ namespace ESPlus.Storage.Raven
     {
         private readonly IDocumentStore _documentStore;
         private Dictionary<string, object> _writeCache = new Dictionary<string, object>();
+        private Dictionary<string, object> _cache = new Dictionary<string, object>();
 
         public RavenStorage(IDocumentStore documentStore)
         {
@@ -16,19 +18,35 @@ namespace ESPlus.Storage.Raven
 
         public void Flush()
         {
-            using (var session = _documentStore.OpenSession())
+            for (var i = 0; ; ++i)
             {
-                foreach (var item in _writeCache)
+                var page = _writeCache.Skip(i * 30).Take(30);
+
+                if (!page.Any())
                 {
-                    session.Store(item.Value, item.Key);
+                    break;
+                }
+
+                using (var session = _documentStore.OpenSession())
+                {
+                    foreach (var item in page)
+                    {
+                        session.Store(item.Value, item.Key);
+                    }
                     session.SaveChanges();
                 }
             }
+
             _writeCache.Clear();
         }
 
         public T Get<T>(string path)
         {
+            if (_cache.ContainsKey(path))
+            {
+                return (T)_cache[path];
+            }
+
             using (var session = _documentStore.OpenSession())
             {
                 return session.Load<T>(path);
@@ -38,6 +56,7 @@ namespace ESPlus.Storage.Raven
         public void Put(string path, object item)
         {
             _writeCache[path] = item;
+            _cache[path] = item;
         }
 
         public void Reset()
