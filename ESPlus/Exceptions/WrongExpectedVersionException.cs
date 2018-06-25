65f9d61e9b150fb017f0ddcec0c967bf30d8e602
Adam Furtenbach <adam.furtenbach@gmail.com>
2018-06-25 23:36:46 +0200

    tests

---
+++ ESPlus/Exceptions/WrongExpectedVersionException.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace ESPlus
+{
+    public class WrongExpectedVersionException : Exception
+    {
+        public WrongExpectedVersionException()
+        {
+        }
+
+        public WrongExpectedVersionException(string message) : base(message)
+        {
+        }
+
+        public WrongExpectedVersionException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected WrongExpectedVersionException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
