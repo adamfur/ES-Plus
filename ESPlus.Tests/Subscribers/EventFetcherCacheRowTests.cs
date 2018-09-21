be0506b6537d4c044ae0fd25e96fd28b67a89561
Adam Furtenbach <adam.furtenbach@gmail.com>
2018-09-21 16:46:47 +0200

    .

--- ESPlus.Tests/Subscribers/EventFetcherCacheRowTests.cs
+++ ESPlus.Tests/Subscribers/EventFetcherCacheRowTests.cs
@@ -6,61 +6,61 @@ using Xunit;
 
 namespace ESPlus.Tests.Subscribers
 {
-    public class EventFetcherCacheRowTests
-    {
-        private EventFetcherCacheRow _row;
+    // public class EventFetcherCacheRowTests
+    // {
+    //     private EventFetcherCacheRow _row;
 
-        public EventFetcherCacheRowTests()
-        {
-            _row = new EventFetcherCacheRow(2L.ToPosition(), 4L.ToPosition(), CreateEvents(2, 4));
-        }
+    //     public EventFetcherCacheRowTests()
+    //     {
+    //         _row = new EventFetcherCacheRow(2L.ToPosition(), 4L.ToPosition(), CreateEvents(2, 4));
+    //     }
 
-        private EventStream CreateEvents(long from, long to)
-        {
-            var result = new List<Event>();
+    //     private EventStream CreateEvents(long from, long to)
+    //     {
+    //         var result = new List<Event>();
 
-            for (var i = from; i <= to; ++i)
-            {
-                result.Add(new Event(null) { Position = i.ToPosition() });
-            }
+    //         for (var i = from; i <= to; ++i)
+    //         {
+    //             result.Add(new Event(null) { Position = i.ToPosition() });
+    //         }
 
-            return new EventStream
-            {
-                Events = result
-            };
-        }
+    //         return new EventStream
+    //         {
+    //             Events = result
+    //         };
+    //     }
 
-        [Theory]
-        [InlineData(2, false)]
-        [InlineData(3, true)]
-        [InlineData(4, true)]
-        [InlineData(6, true)]
-        [InlineData(7, false)]
-        [InlineData(8, false)]
-        public void Within_Theory_AsExcepted(long digit, bool inRange)
-        {
-            var row = new EventFetcherCacheRow(3L.ToPosition(), 7L.ToPosition(), CreateEvents(3, 7));
+    //     [Theory]
+    //     [InlineData(2, false)]
+    //     [InlineData(3, true)]
+    //     [InlineData(4, true)]
+    //     [InlineData(6, true)]
+    //     [InlineData(7, false)]
+    //     [InlineData(8, false)]
+    //     public void Within_Theory_AsExcepted(long digit, bool inRange)
+    //     {
+    //         var row = new EventFetcherCacheRow(3L.ToPosition(), 7L.ToPosition(), CreateEvents(3, 7));
 
-            var result = row.Within(digit.ToPosition());
+    //         var result = row.Within(digit.ToPosition());
 
-            Assert.Equal(inRange, result);
-        }
+    //         Assert.Equal(inRange, result);
+    //     }
 
-        [Fact]
-        public void Select_Theory_As_Expected()
-        {
-            var result = _row.Select(3L.ToPosition()).Events.ToList();
+    //     [Fact]
+    //     public void Select_Theory_As_Expected()
+    //     {
+    //         var result = _row.Select(3L.ToPosition()).Events.ToList();
 
-            Assert.Equal(3L.ToPosition(), result[0].Position);
-            Assert.Equal(4L.ToPosition(), result[1].Position);
-        }
+    //         Assert.Equal(3L.ToPosition(), result[0].Position);
+    //         Assert.Equal(4L.ToPosition(), result[1].Position);
+    //     }
 
-        // [Fact]
-        // public void Merge_adasd_asdasd()
-        // {
-        //     _row.Merge(CreateEvents(3, 5));
+    //     // [Fact]
+    //     // public void Merge_adasd_asdasd()
+    //     // {
+    //     //     _row.Merge(CreateEvents(3, 5));
 
-        //     Assert.Equal(4, _row.Select(0).Count());
-        // }
-    }
+    //     //     Assert.Equal(4, _row.Select(0).Count());
+    //     // }
+    // }
 }
