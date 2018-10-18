fce8b0690c9b06c896a98586897673940bb916f9
Adam Furtenbach <adam.furtenbach@gmail.com>
2018-10-18 13:33:22 +0200

    .

--- ESPlus/Subscribers/Event.cs
+++ ESPlus/Subscribers/Event.cs
@@ -7,8 +7,11 @@ namespace ESPlus.Subscribers
     public class Event
     {
         private readonly IEventTypeResolver _eventTypeResolver;
-        private object _cache;
-        private object _mutex = new object();
+        public Position Position { get; set; }
+        public byte[] Meta { get; set; }
+        public byte[] Payload { get; set; }
+        public string EventType { get; set; }
+        public bool IsAhead { get; set; } = false;
 
         public Event(IEventTypeResolver eventTypeResolver)
         {
@@ -18,35 +21,14 @@ namespace ESPlus.Subscribers
         public Event(bool isAhead)
         {
             IsAhead = isAhead;
-        }        
+        }
 
         public object DeserializedItem()
         {
-            if (_cache != null)
-            {
-                return _cache;
-            }
+            var json = Encoding.UTF8.GetString(Payload);
+            var result = JsonConvert.DeserializeObject(json, _eventTypeResolver.ResolveType(EventType));
 
-            lock (_mutex)
-            {
-                if (_cache != null)
-                {
-                    return _cache;
-                }
-                
-                var json = Encoding.UTF8.GetString(Payload);
-                var result = JsonConvert.DeserializeObject(json, _eventTypeResolver.ResolveType(EventType));
-
-                _cache = result;
-            }
-            return _cache;
+            return result;
         }
-
-
-        public Position Position { get; set; }
-        public byte[] Meta { get; set; }
-        public byte[] Payload { get; set; }
-        public string EventType { get; set; }
-        public bool IsAhead { get; set; } = false;
     }
 }
