06aa6b96b7d6146a783dddbdfc88192107f5f051
Adam Furtenbach <adam.furtenbach@gmail.com>
2019-12-17 12:24:52 +0100

    remove latency from FlushWhenAheadPolicy

--- ESPlus/FlushPolicies/FlushWhenAheadPolicy.cs
+++ ESPlus/FlushPolicies/FlushWhenAheadPolicy.cs
@@ -9,44 +9,12 @@ namespace ESPlus.FlushPolicies
     {
         public IEventHandler EventHandler { get; set; }
         private readonly TimeSpan _timeout;
-        private readonly object _mutex = new object();
-        private bool _doFlush = false;
         private const int _eventThreshold = 100;
         private int _events = 0;
 
-        public FlushWhenAheadPolicy(TimeSpan timeout)
-        {
-            _timeout = timeout;
-        }
-
-        public void Start()
-        {
-            new Thread(Worker).Start();
-        }
-
-        private void Worker()
-        {
-            while (true)
-            {
-                lock (_mutex)
-                {
-                    while (!_doFlush)
-                    {
-                        Monitor.Wait(_mutex);
-                    }
-                }
-                Thread.Sleep(_timeout);
-                Flush();
-            }
-        }
-
         public void FlushWhenAhead()
         {
-            lock (_mutex)
-            {
-                _doFlush = true;
-                Monitor.Pulse(_mutex);
-            }
+            Flush();
         }
 
         public void FlushEndOfBatch()
@@ -57,23 +25,86 @@ namespace ESPlus.FlushPolicies
         {
             if (++_events > _eventThreshold)
             {
-                lock (_mutex)
-                {
-                    _doFlush = true;
-                    Monitor.Pulse(_mutex);
-                }
+                Flush();
             }
         }
 
         private void Flush()
         {
-            lock (_mutex)
-            {
-                _doFlush = false;
-            }
-
             _events = 0;
             EventHandler.Flush();
         }
     }
+    
+//    public class FlushWhenAheadPolicy : IFlushPolicy
+//    {
+//        public IEventHandler EventHandler { get; set; }
+//        private readonly TimeSpan _timeout;
+//        private readonly object _mutex = new object();
+//        private bool _doFlush = false;
+//        private const int _eventThreshold = 100;
+//        private int _events = 0;
+//
+//        public FlushWhenAheadPolicy(TimeSpan timeout)
+//        {
+//            _timeout = timeout;
+//        }
+//
+//        public void Start()
+//        {
+//            new Thread(Worker).Start();
+//        }
+//
+//        private void Worker()
+//        {
+//            while (true)
+//            {
+//                lock (_mutex)
+//                {
+//                    while (!_doFlush)
+//                    {
+//                        Monitor.Wait(_mutex);
+//                    }
+//                }
+//                Thread.Sleep(_timeout);
+//                Flush();
+//            }
+//        }
+//
+//        public void FlushWhenAhead()
+//        {
+//            lock (_mutex)
+//            {
+//                _doFlush = true;
+//                Monitor.Pulse(_mutex);
+//            }
+//        }
+//
+//        public void FlushEndOfBatch()
+//        {
+//        }
+//
+//        public void FlushOnEvent()
+//        {
+//            if (++_events > _eventThreshold)
+//            {
+//                lock (_mutex)
+//                {
+//                    _doFlush = true;
+//                    Monitor.Pulse(_mutex);
+//                }
+//            }
+//        }
+//
+//        private void Flush()
+//        {
+//            lock (_mutex)
+//            {
+//                _doFlush = false;
+//            }
+//
+//            _events = 0;
+//            EventHandler.Flush();
+//        }
+//    }
 }
